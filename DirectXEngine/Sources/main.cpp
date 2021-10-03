fc47eb35a74fe8e866b398b5ee0f826254b74fb3
Alexander Sukhikh <xbox24_772@mail.ru>
2021-10-03 21:18:12 +0300

    Fix debug msg

--- DirectXEngine/Sources/main.cpp
+++ DirectXEngine/Sources/main.cpp
@@ -1,10 +1,12 @@
 #include <Windows.h>
 #include "Engine.h"
 
-int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+int APIENTRY wWinMain(
+	_In_ HINSTANCE hInstance,
 	_In_opt_ HINSTANCE hPrevInstance,
 	_In_ LPWSTR lpCmdLine,
-	_In_ int nCmdShow) {
+	_In_ int nCmdShow
+) {
 
 	HRESULT hr = CoInitialize(nullptr);
 	ONFAILHRLOG(hr, "Failed to call CoInitialize", -1);
@@ -17,7 +19,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 		}
 	}
 	else {
-		OutputDebugStringA("Fuck\n");
+		OutputDebugStringA("Failed to initialize engine\n");
 	}
 	return 0; 
 }
