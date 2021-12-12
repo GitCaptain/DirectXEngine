8b7f2cb039e1cb6243327ad7e21e57043eb495da
Alexander Sukhikh <alexsm.rf24@gmail.com>
2021-12-13 01:12:01 +0300

    Fix specular calculation in shaders Rename gloabal constant buffer variables

--- DirectXEngine/Resources/Shaders/PS/deferred_light_pass_ps.hlsl
+++ DirectXEngine/Resources/Shaders/PS/deferred_light_pass_ps.hlsl
@@ -5,23 +5,25 @@
 Texture2D positionTex:        register(t0);
 Texture2D colorTex:           register(t1);
 Texture2D normalTex:          register(t2);
-Texture1D lightTexture:       register(t3);
+Texture2D specularTex:        register(t3);
+Texture1D lightTexture:       register(t4);
 
 float4 main(float4 screenPos: SV_POSITION) : SV_TARGET{
     float3 texCoord = float3(screenPos.xy, 0);
     float3 sampleColor = colorTex.Load(texCoord);
     float3 normal = normalTex.Load(texCoord);
     float3 worldPosition = positionTex.Load(texCoord);
+    float4 specular = specularTex.Load(texCoord);
 
-    float4 lightedColor = calculateLightTex(worldPosition, sampleColor, normal, lightTexture);
-    if (hdr) {
-        if (exposure >= 0.01f) {
-            lightedColor = toneMap(lightedColor, exposure);
+    float4 lightedColor = calculateLightTex(worldPosition, sampleColor, normal, specular.rgb, 1.0, specular.a, lightTexture);
+    if (cb_hdr) {
+        if (cb_exposure >= 0.01f) {
+            lightedColor = toneMap(lightedColor, cb_exposure);
         }
         else {
             lightedColor = toneMap(lightedColor);
         }
     }
-    float4 correctedColor = getGammaCorrectedColor(lightedColor, gamma);
+    float4 correctedColor = getGammaCorrectedColor(lightedColor, cb_gamma);
     return correctedColor;
 }
