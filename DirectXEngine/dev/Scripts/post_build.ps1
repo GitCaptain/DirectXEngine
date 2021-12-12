0e4de779f2d5f8cf72fba1e317c8164cee23e0a8
Alexander Sukhikh <alexsm.rf24@gmail.com>
2021-12-12 20:33:39 +0300

    Fix post-build script: copy resources to the output directory

--- DirectXEngine/dev/Scripts/post_build.ps1
+++ DirectXEngine/dev/Scripts/post_build.ps1
@@ -1,16 +1,28 @@
-#copy libraries to build directory
-
 param ($Platform, $Config, $OutDir)
 
 # .... i love powershell
 # didn't found proper way to pass $DevEnvDir var without all this quotes
 function Fix-Parameter ($Param) {
-	return $Param -replace "'", ""
+    return $Param -replace "'", ""
 }
 
 $Platform = Fix-Parameter $Platform
 $Config = Fix-Parameter $Config
 $OutDir = Fix-Parameter $OutDir
 
+#copy libraries to build directory
 Copy-Item "$PSScriptRoot\..\Libs\$Platform\$Config\*" "$OutDir" -Recurse
 Copy-Item "$PSScriptRoot\..\Libs\Any\*" "$OutDir" -Recurse
+
+#copy resources to build directory
+$ResourcesDir = "$PSScriptRoot\..\..\Resources"
+$ResourceSubdirs = ('Fonts', 'Objects', 'Textures')
+foreach ($ResourceSubdir in $ResourceSubdirs) {
+    Copy-Item "$ResourcesDir\$ResourceSubdir" "$OutDir\Resources\$ResourceSubdir" -Recurse
+}
+
+#copy builded shaders to build directory
+# save internal directory structure
+$ShadersDir = "$OutDir\$Platform\$Config"
+mkdir -Force "$ShadersDir"
+Copy-Item "$OutDir\*.cso" "$ShadersDir" -Recurse
